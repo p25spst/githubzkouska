0f095c23d64264a283329ace5a17869035b8e5d0
p25.test <pisemka1.test@gmail.com>
2021-05-19 13:07:03 +0200

    Přidejte soubory projektu.

---
+++ githubzkouska/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace githubzkouska
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
